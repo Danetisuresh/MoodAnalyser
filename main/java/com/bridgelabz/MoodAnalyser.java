52b632b9aafa7bc23fb8077eb75a3b12291eebda
suresh Daneti <sureshdaneti99@gmail.com>
2021-10-17 18:19:57 +0530

    Added: I am in sad Mood message should Return Happy

--- main/java/com/bridgelabz/MoodAnalyser.java
+++ main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,13 +1,15 @@
 package com.bridgelabz;
 /**
- * Purpose  - “I am in Sad Mood” message Should Return SAD
+ * Purpose  - “I am in Sad Mood” message Should Return HAPPY
  */
 
 public class MoodAnalyser {
 	public static String analyseMood(String message) {
 		if (message.toLowerCase().contains("sad")) {
-		}
 		return "Sad";
 	}
-
+  else if (message.toLowerCase().contains("Happy")) {
+  }
+	return "Happy";
+ }
 }
